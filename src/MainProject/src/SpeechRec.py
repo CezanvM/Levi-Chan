c7f9c53ed6ba2c4de2f323c28f97b8a2ce9a5e5f
Lars <ldijk6@avans.nl>
2018-12-06 13:59:03 +0100

    Speech-to-text and audio Implemented
    - added Speech-to-text implementation
    - added Microphone recording
    - changed environmentMain to environment

--- src/MainProject/src/SpeechRec.py
+++ src/MainProject/src/SpeechRec.py
@@ -5,38 +5,34 @@ import pandas as pd
 import datetime
 import json
 from playsound import playsound
-print(sr.__version__)
 
 APIKey = "HOFLYUVHDMKRL6ZWGYM6UZ6I5B65A6P5"
 waitingOnAPI = False
-#soundfile = "harvard.wav"
-soundfile = "jackhammer.wav" #currently not imported since its unnesesary when audo recording works
 
 r = sr.Recognizer()
+mic = sr.Microphone()
 
 def recLoop():
-	print("listering")
-	# TODO Actual audio recording
-	audio = sr.AudioFile(soundfile)
+	print("Listening")
+	#microphone listining
+	with mic as source:
+		r.adjust_for_ambient_noise(source, duration=0.5)
+		audio = r.listen(source)
+
+	print("Done listening")
+
 	if waitingOnAPI == False:
 		sendAudioToAPI(audio)
-
-
+		
 def SpeechStartup():
-    print("speech started")
-    recLoop()
-
-
+	recLoop()
+	
 def sendAudioToAPI(sound):
 	waitingOnAPI = True
-
-	#converting audio to correct type
-	with sound as source:
-		r.adjust_for_ambient_noise(source, duration=0.5)
-		audio = r.record(source, duration=4)
-	
 	#sending audio to API
-	sentence =  r.recognize_wit(audio, APIKey)
+	# TODO Check if working with bad english
+	sentence =  r.recognize_wit(sound, APIKey)
+	#sentence =  r.recognize_google(sound) #used for basic audio testing, conversion testing done with WIT.ai
 	APICallback(sentence)
 
 
